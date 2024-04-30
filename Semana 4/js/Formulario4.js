b880ebdb0ff8df588adea59ccaa0d0e5bd03905c
Uluru92 <jorddy.castro@gmail.com>
2024-04-29 20:42:17 -0600

    Formulario 4 DONE

--- Semana 4/js/Formulario4.js
+++ Semana 4/js/Formulario4.js
@@ -1,23 +1,21 @@
 /*Voy a hacer el load de la pagina*/
 $(document).ready(function () {
-    /* Guardar el boton en una variable */
-    /* Capturar las propiedades de la etiqueta botonCapturarDato */
-    var botonCapturarData = document.getElementById("botonSumar");
+
+    var botonCapturarData = document.getElementById("botonEdad");
     console.log(botonCapturarData)
-
-    /* Al botonCapturarDato le voya agregar un evento que es de tipo clic */
     botonCapturarData.addEventListener("click", function () {
-
-        /* Capturar no los atributos, solo el atributo valor de la etiqueta*/
-        var var1 = document.getElementById("valor1").value;
-        var var2 = document.getElementById("valor2").value;
-
-        var1 = parseFloat(var1)
-        var2 = parseFloat(var2)
-        var var3 = var1 + var2
-        alert("Resultado: " + var3)
-        console.log("Resultado: ", var3)
-
+        var edad = document.getElementById("edad").value;
+        var edad = parseFloat(edad)
+        if (edad < 18)
+        {
+            alert("Es menor de edad")
+            console.log("Es menor de edad")
+        }
+        else
+        {
+            alert("Es mayor de edad")
+            console.log("Es mayor de edad")
+        }
     });
 });
 
---
+++ Semana 4/js/clase 1 java.js
@@ -0,0 +1,17 @@
+var valor1 = 1
+var valor2 = 2
+var valor3 = 3
+var valor4 = 5
+
+if (valor4 == valor1) {
+    console.log("Estudiante Regular")
+}
+else if (valor4 == valor2) {
+    console.log("Estudiante Becado")
+}
+else if (valor4 == valor3) {
+    console.log("Estudiante Descuento")
+}
+else {
+    console.log("Opción inválida")
+}
