17a0c0670b366a0a1f5a0d66f14736eab4015637
Uluru92 <jorddy.castro@gmail.com>
2024-05-20 09:50:24 -0600

    update practice java

--- Java_practica.js/recorrer_array.js
+++ Java_practica.js/recorrer_array.js
@@ -1,14 +1,30 @@
+/*Crear un Array*/
 let frutas = ["Manzana", "Banana"]; 
 console.log(frutas.length);
 
-let primero = frutas[0];
+/*Acceder a un elemento de Array mediante su índice*/
+primero = frutas[0];
+console.log(primero)
 // Manzana
-
-let ultimo = frutas[frutas.length - 1];
+ultimo = frutas[frutas.length - 1];
 // Banana
 
+/*Recorrer un Array*/
 frutas.forEach(function (elemento, indice, array) {
     console.log(elemento, indice);
 });
 // Manzana 0
 // Banana 1
+
+/*Añadir un elemento al final de un Array*/
+nuevaLongitud = frutas.push("Naranja"); // Añade "Naranja" al final
+// ["Manzana", "Banana", "Naranja"]
+
+/*Eliminar el último elemento de un Array*/
+let ultimo = frutas.pop(); // Elimina "Naranja" del final
+// ["Manzana", "Banana"]
+
+/*Añadir un elemento al principio de un Array*/
+let nuevaLongitud = frutas.unshift("Fresa"); // Añade "Fresa" al inicio
+console.log(nuevaLongitud)
+// ["Fresa" ,"Manzana", "Banana"]
