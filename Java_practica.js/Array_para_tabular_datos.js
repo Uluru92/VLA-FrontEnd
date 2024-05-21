f58601a08e253420b7c7becd59e601bf81f5b989
Uluru92 <jorddy.castro@gmail.com>
2024-05-21 13:58:39 -0600

    Uso de un array para tabular un conjunto de valores

--- Java_practica.js/Array_para_tabular_datos.js
+++ Java_practica.js/Array_para_tabular_datos.js
@@ -1,5 +1,5 @@
 valores = [];
 for (let x = 0; x < 10; x++) {
-    valores.push([2 ** x, 2 * x ** 2]);
+    valores.push([2 ** x, 2 * x ** 2]);// primer valor: 2 elevado a la x, segundo valor 2 * x al cuadrado
 }
 console.table(valores);
