0a133bfc9819094f04268bdab74874a7419f8329
Uluru92 <jorddy.castro@gmail.com>
2024-06-12 16:13:49 -0600

    update clase flex

--- Tarea2/ejercicio2_DONE.js
+++ Tarea2/ejercicio2_DONE.js
@@ -9,11 +9,12 @@ y mayor a 35 es un adulto.
 const prompt = require('prompt-sync')()
 var age = NaN
 
-while (isNaN(age)) {
+while (isNaN(age))
+{
     var age = parseFloat(prompt("Please enter your age: "))
     if (0 < age && age < 13) { console.log("You are", age, "years old, you are a child") }                /*Niño*/
     else if (13 <= age && age < 20) { console.log("You are", age, "years old, you are a teenager") }      /*adolescente*/
     else if (20 <= age && age <= 35) { console.log("You are", age, "years old, you are a young adult") }  /*adulto joven*/
     else if (age > 35) { console.log("You are", age, "years old, you are an adult") }                     /*adulto*/
     else { console.log("lets try again!... make sure you enter your age in numbers, not words") }               
-} 
+}
