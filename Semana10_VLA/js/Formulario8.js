dbedfa59d80552835697d326ef6db825ee340f45
Uluru92 <jorddy.castro@gmail.com>
2024-06-14 16:04:43 -0600

    update files semana10

--- Semana10_VLA/js/Formulario8.js
+++ Semana10_VLA/js/Formulario8.js
@@ -6,44 +6,26 @@ $(document).ready(function()
     {
         let valor1 = parseFloat(document.getElementById('valor1').value);
         let valor2 = parseFloat(document.getElementById('valor2').value);
-        let operacion = document.getElementById('tipoOperacion').value;
+        let operacion = document.getElementById('tipoOperacion').value; 
+            
+        let valor1Validado = ValidarSoloNumeros(valor1);
+        let valor2Validado = ValidarSoloNumeros(valor2);
 
-        //Validar que valor 1 y valor 2 no sean vacios
-        let camposValidados = ValidarCamposEntrada(valor1, valor2);
-
-        if(camposValidados)
+        if(valor1Validado || valor2Validado)
         {
-            document.getElementById('mensaje').innerHTML = "Campos no pueden ser vacios";
+            document.getElementById('mensaje').innerHTML = "Solo se pueden numeros";
         }
         else
         {
-            let valor1Validado = ValidarSoloNumeros(valor1);
-            let valor2Validado = ValidarSoloNumeros(valor2);
-
-            if(valor1Validado || valor2Validado)
-            {
-                document.getElementById('mensaje').innerHTML = "Solo se pueden numeros";
-            }
-            else
-            {
-                let resultado = EjecutarOperacion(valor1, valor2, operacion);
-                document.getElementById('mensaje').innerHTML = resultado;
-            }
-        }        
+            let resultado = EjecutarOperacion(valor1, valor2, operacion);
+            document.getElementById('mensaje').innerHTML = resultado;
+        }              
     });    
 });
 
 function ValidarSoloNumeros( valor)
 {
     if(isNaN(valor))
-        return true;
-    else
-        return false;
-}
-
-function ValidarCamposEntrada(v1, v2)
-{
-    if(v1 =='' || v2 =='')
         return true;
     else
         return false;
