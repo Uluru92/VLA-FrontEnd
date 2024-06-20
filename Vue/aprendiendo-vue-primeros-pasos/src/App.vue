140bd76d80f63b164f4268cc4f9e383117fe4a2c
Uluru92 <jorddy.castro@gmail.com>
2024-06-19 19:56:09 -0600

    Aprendiendo Vue... componentes

---
+++ Vue/aprendiendo-vue-primeros-pasos/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <SaludoMundo
+  NombrePersona = 'Adolfo'
+  EdadPersona = 31
+  />
+  <SaludoMundo
+  NombrePersona = 'Jorddy'
+  EdadPersona = 32s
+  />
+  <SaludoMundo
+  NombrePersona = 'Kimberly'
+  EdadPersona = '33s'
+  />
+
+</template>
+
+<script>
+  import SaludoMundo from './components/SaludoMundo.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    SaludoMundo
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Vue/aprendiendo-vue-primeros-pasos/src/components/SaludoMundo.vue
@@ -0,0 +1,25 @@
+<template>
+    <!-- Todo el html que yo quiera-->
+     <h1> Estoy saludando, estoy vivo</h1>
+     <h1>Mi nombre es {{NombrePersona}}</h1>
+     <h1>Tengo: {{EdadPersona}} anios</h1>
+
+</template>
+
+
+<script>
+/* Todo el javascript del componente */
+    export default
+    {
+        props:
+        {
+            NombrePersona: String,
+            EdadPersona: Number
+        }  
+    }
+
+</script>
+
+<style>
+
+</style>
