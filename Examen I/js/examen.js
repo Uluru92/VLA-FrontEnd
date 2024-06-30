d990364f38fc7b5ba552295a93c4efe6fa386cdb
Uluru92 <jorddy.castro@gmail.com>
2024-06-30 10:15:00 -0600

    update examen I: Inicio Listo

--- Examen I/js/examen.js
+++ Examen I/js/examen.js
@@ -1,3 +1,14 @@
 $(function () {
     $('#menu-placeholder').load('menu.html');
 })
+
+
+
+//* carousel...
+//const myCarouselElement = document.querySelector('myCarousel')
+
+//const carousel = new bootstrap.Carousel(myCarouselElement, {
+//    interval: 2000,
+//    touch: true
+//})
+//
