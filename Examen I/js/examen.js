8b1abd028f97a3b84ecb34a0d51f84cf2da6e90f
Uluru92 <jorddy.castro@gmail.com>
2024-06-30 21:19:46 -0600

    contador de letras DONE

--- Examen I/js/examen.js
+++ Examen I/js/examen.js
@@ -2,13 +2,35 @@ $(function () {
     $('#menu-placeholder').load('menu.html');
 })
 
+$(document).ready(function () {
+
+    let botonEnviar = document.getElementById("botonEnviarInfo");
+    
+    botonEnviar.addEventListener("click", function () {
+        var nombre = document.getElementById("nombrePersona").value;
+        var primerApellido = document.getElementById("primerApellidoPersona").value;
+        var segundoApellido = document.getElementById("segundoApellidoPersona").value;
+        var correo = document.getElementById("correoPersona").value;
+        var telefono = document.getElementById("telefonoPersona").value;
+        var nacimiento = document.getElementById("nacimiento").value;
+        var comentario = document.getElementById("comentarioAdicional").value;
+
+        if (nombre == "")           
+
+        if (primerApellido == "")
+
+        if (segundoApellido == "")
+
+        if (correo == "")
+
+        if (telefono == "")
+
+        if (nacimiento == "")
+
+        if (comentario == "")
+
+    })
 
 
-//* carousel...
-//const myCarouselElement = document.querySelector('myCarousel')
-
-//const carousel = new bootstrap.Carousel(myCarouselElement, {
-//    interval: 2000,
-//    touch: true
-//})
-//
+    }
+)
