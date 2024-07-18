3b3fff3e9b82f48d310bce247ba931064f441ad5
Uluru92 <jorddy.castro@gmail.com>
2024-07-17 20:08:48 -0600

    ejercicio 10 ts

---
+++ AprendiendoTypeScript/Ejercicio10.ts
@@ -0,0 +1,21 @@
+class personaCarro {
+    nombre: string = "";
+    cedula: number = 0;
+    listaCarros: carro[] = [];
+}
+
+class carro {
+    placa: number;
+    constructor(pplaca: number) {
+        this.placa = pplaca;
+    }
+}
+
+const persona1 = new personaCarro();
+persona1.nombre = "Jorddy";
+persona1.cedula = 114980956;
+
+const carro1persona1 = new carro (123456);
+persona.listaCarros.push(carro1persona1);
+
+console.log("Nombre:", persona1.nombre, "Placa carro asosiado: ", carro1persona1.placa);
