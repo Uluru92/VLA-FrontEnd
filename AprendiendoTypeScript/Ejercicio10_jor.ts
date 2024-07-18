3c76abc4f524c8720e6fdbdd3d62ff405f12aa7e
Uluru92 <jorddy.castro@gmail.com>
2024-07-17 20:16:10 -0600

    ejercicios solucionados por mi renombrados

--- AprendiendoTypeScript/Ejercicio10_jor.ts
+++ AprendiendoTypeScript/Ejercicio10_jor.ts
@@ -16,6 +16,6 @@ persona1.nombre = "Jorddy";
 persona1.cedula = 114980956;
 
 const carro1persona1 = new carro (123456);
-persona.listaCarros.push(carro1persona1);
+persona1.listaCarros.push(carro1persona1);
 
 console.log("Nombre:", persona1.nombre, "Placa carro asosiado: ", carro1persona1.placa);
