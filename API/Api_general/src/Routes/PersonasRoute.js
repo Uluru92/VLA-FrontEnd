a4cfc81f886627a4f1d7714d57d9232b9cb1fbcf
Uluru92 <jorddy.castro@gmail.com>
2024-08-01 11:59:28 -0600

    reorganización

--- API/Api_general/src/Routes/PersonasRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const ObtenerTodasLasPersonas = require("../Controlers/PersonaController");
-router
-    .get("/ObtenerTodasLasPersonas", ObtenerTodasLasPersonas.ObtenerTodasLasPersonas)
-
-module.exports = router;
