314d9bf9324b7cc540e4cd38e327c5c66619d903
Uluru92 <jorddy.castro@gmail.com>
2024-08-08 10:06:03 -0600

    update vue docs

--- clase-repaso/src/views/AboutView.vue
+++ clase-repaso/src/views/AboutView.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is an about pagee</h1>
+
+    tabla con las columnas y apellido 
+    usted crear un componente que se llame formulario Basico
+    input nombre
+    input apellidoboton Registrar En Tabla
+
+
   </div>
 </template>
