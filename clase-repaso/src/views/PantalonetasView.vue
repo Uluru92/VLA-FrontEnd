265d12effda7b7a49b69dcad8445a9dae44bc9b8
Uluru92 <jorddy.castro@gmail.com>
2024-08-07 18:31:58 -0600

    clase repaso update

---
+++ clase-repaso/src/views/PantalonetasView.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>Estoy en la vista de pantalonetas</h1>
+    <SeleccionPantalonetaComponent></SeleccionPantalonetaComponent>
+    
+</template>
+
+<script lang="ts">
+    import {defineComponent} from 'vue'
+    import SeleccionPantalonetaComponent from '@/components/SeleccionPantalonetasComponent.vue'
+
+
+    export default defineComponent ({
+        components:
+        {
+            SeleccionPantalonetaComponent 
+        }
+
+    })
+</script>
