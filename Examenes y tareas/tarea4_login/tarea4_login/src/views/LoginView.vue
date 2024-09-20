4616a4e4ac2b69e4381d0090b212aafa9ffbc3ba
Uluru92 <jorddy.castro@gmail.com>
2024-09-20 01:55:33 -0600

    indicaciones tarea 4

--- Examenes y tareas/tarea4_login/tarea4_login/src/views/LoginView.vue
+++ Examenes y tareas/tarea4_login/tarea4_login/src/views/LoginView.vue
@@ -1,5 +1,19 @@
+<!--
+Tarea 4
+Realizar la implementación de la vista (pantalla) Login, en el cuál se requieren los siguientes campos:
+
+    - Nombre de usuario
+    - Contraseña
+     -Botón de validar
+
+Importante mencionar que ambos valores son requeridos para realizar la validación de si el usuario
+existe o no. Como no hemos aprendido el tema de manejo de API, se requiere que al momento de validar
+los campos, se permita solo el usuario admin y la contraseña 123*, muestre con un alert si el usuario
+es permitido o no.
+-->
+
 <template>
-    <h1>Estoy en Login</h1>
+    <h1>HolaEstoy en Login</h1>
 </template>
 
 <script lang='ts'>
