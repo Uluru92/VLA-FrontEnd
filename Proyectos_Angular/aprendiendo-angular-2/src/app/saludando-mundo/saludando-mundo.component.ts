bb1e6c1e75528219899246309299260b3f6c7043
Uluru92 <jorddy.castro@gmail.com>
2024-10-24 14:40:24 -0600

    creacion de 1er componente prueba

---
+++ Proyectos_Angular/aprendiendo-angular-2/src/app/saludando-mundo/saludando-mundo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'saludando-mundo',
+  standalone: true,
+  imports: [],
+  templateUrl: './saludando-mundo.component.html',
+  styleUrl: './saludando-mundo.component.scss'
+})
+export class SaludandoMundoComponent {
+
+}
