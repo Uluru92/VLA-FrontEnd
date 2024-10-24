6293020568875b54bde9b6bc00bcf543d61d366c
Uluru92 <jorddy.castro@gmail.com>
2024-10-24 15:33:05 -0600

    borrando los primeros proyectos de prueba y abriendo nuevo proyecto no standalone

--- Proyectos_Angular/primeros-pasos-angular/src/app/app.component.spec.ts
+++ Proyectos_Angular/primeros-pasos-angular/src/app/app.component.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        RouterModule.forRoot([])
+      ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   });
 
@@ -14,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'aprendiendo-angular' title`, () => {
+  it(`should have as title 'primeros-pasos-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('aprendiendo-angular');
+    expect(app.title).toEqual('primeros-pasos-angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, aprendiendo-angular');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, primeros-pasos-angular');
   });
 });
