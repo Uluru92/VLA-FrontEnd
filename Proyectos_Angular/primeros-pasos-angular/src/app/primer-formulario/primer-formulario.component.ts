fd0c4c2f1f4229614774049502cf9e52576d5f9f
Uluru92 <jorddy.castro@gmail.com>
2024-10-26 00:28:39 -0600

    ngif implemented

--- Proyectos_Angular/primeros-pasos-angular/src/app/primer-formulario/primer-formulario.component.ts
+++ Proyectos_Angular/primeros-pasos-angular/src/app/primer-formulario/primer-formulario.component.ts
@@ -10,8 +10,26 @@ export class PrimerFormularioComponent
   nombrePersona: string = "";
   apellidoPersona: string = "";
   edadPersona: string = "";
+  mostrarMensajeErrorNombre: boolean = false;
+  mostrarMensajeErrorApellido: boolean = false;
+  mostrarMensajeErrorEdad: boolean = false;
+
 
   ValidarCampos(): void{
-    alert(this.nombrePersona)
+    
+    if (this.nombrePersona == "")
+      this.mostrarMensajeErrorNombre = true;
+    else
+      this.mostrarMensajeErrorNombre = false;
+
+    if (this.apellidoPersona == "")
+      this.mostrarMensajeErrorApellido = true;
+    else
+      this.mostrarMensajeErrorApellido = false;
+
+    if (this.edadPersona == "")
+      this.mostrarMensajeErrorEdad = true;
+    else
+      this.mostrarMensajeErrorEdad = false;
   }
 }
