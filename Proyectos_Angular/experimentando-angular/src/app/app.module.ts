fc2841204cbc193026074ce75e9f4c636f1989a6
Uluru92 <jorddy.castro@gmail.com>
2024-11-06 16:17:48 -0600

    importacion del NgbModule

--- Proyectos_Angular/experimentando-angular/src/app/app.module.ts
+++ Proyectos_Angular/experimentando-angular/src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
 import { GeneralComponent } from './general/general.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ConsumoApiComponent } from './consumo-api/consumo-api.component';
 import { DirectivasComponent } from './directivas/directivas.component';
 import { FormularioUnoComponent } from './formulario-uno/formulario-uno.component';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-//import { NgModule } from '@ng-bootstrap/ng-bootstrap'
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
 
 @NgModule({
   declarations: [
@@ -23,7 +25,7 @@ import { HttpClientModule } from '@angular/common/http';
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
-  //  NgModule
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
