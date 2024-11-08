1b4dfddefe7c3494f988a2a39b2bf3adb4c97229
Uluru92 <jorddy.castro@gmail.com>
2024-11-07 23:23:16 -0600

    edicion class Pais completed

--- Proyectos_Angular/experientando-angular_clase_42/experientando-angular/src/app/models/Pais.ts
+++ Proyectos_Angular/experientando-angular_clase_42/experientando-angular/src/app/models/Pais.ts
@@ -2,9 +2,36 @@ export class Pais
 {
     idPais : string = "";
     nombrePais: string = "";
+    capital: string = "";
+    moneda: string = "";
+    poblacion: string = "";
+    presidente: string = "";
+    limitaCon: string = "";
+    noticiasRelevante: string = "";
+    lugaresTuristicos: string = "";
+    videoLink: string = ""
 
-    constructor(pIdPais: string, pNombrePais: string){
+    constructor(
+        pIdPais: string,
+        pNombrePais: string,
+        pCapital: string,
+        pMoneda: string,
+        pPoblacion: string,
+        pPresidente: string,
+        pLimitaCon: string,
+        pNoticiasRelevantes: string,
+        pLugaresTuristicos: string,
+        pVideoLink: string,
+    ) {
         this.idPais = pIdPais;
         this.nombrePais = pNombrePais;
+        this.capital = pCapital;
+        this.moneda = pMoneda;
+        this.poblacion = pPoblacion;
+        this.presidente = pPresidente;
+        this.limitaCon = pLimitaCon;
+        this.noticiasRelevante = pNoticiasRelevantes;
+        this.lugaresTuristicos = pLugaresTuristicos;
+        this.videoLink = pVideoLink;
     }
 }
