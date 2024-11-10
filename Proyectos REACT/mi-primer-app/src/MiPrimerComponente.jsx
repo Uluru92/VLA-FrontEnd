d7754ce5fc84c82ff3981a294e38521e36dd8bb0
Uluru92 <jorddy.castro@gmail.com>
2024-11-09 23:37:54 -0600

    mi primer componente creado en react

---
+++ Proyectos REACT/mi-primer-app/src/MiPrimerComponente.jsx
@@ -0,0 +1,12 @@
+function MiPrimerComponente()
+{
+    return (
+        <div>
+            <h1>Estoy en mi primer componente</h1>
+            <h4>Esto está vacilón!</h4>
+        </div>
+    )    
+
+}
+
+export default MiPrimerComponente;
