ee172b0ea6573663671b14a85e1d481894d1da58
Uluru92 <jorddy.castro@gmail.com>
2024-11-09 23:49:43 -0600

    primera aproximacion react finished

--- Proyectos REACT/mi-primer-app/src/App.js
+++ Proyectos REACT/mi-primer-app/src/App.js
@@ -3,12 +3,17 @@ import './App.css';
 import MiPrimerComponente from './components/MiPrimerComponente';
 
 function App() {
+
+  function ManejarClic() {
+    alert("Usted me ha dado clic");
+    }
+
   return (
     <div>
       <MiPrimerComponente
         Nombre="Jorddy"
         Apellido1="Castro"
-        Apellido2= "Araya"
+        Apellido2="Araya"
       ></MiPrimerComponente>
     </div>
   );
--- Proyectos REACT/mi-primer-app/src/components/MiPrimerComponente.jsx
+++ Proyectos REACT/mi-primer-app/src/components/MiPrimerComponente.jsx
@@ -5,6 +5,8 @@ function MiPrimerComponente(props)
             <h1>Estoy en mi primer componente</h1>
             <h4>Esto está vacilón!</h4>
             <h3>Mi nombre es: {props.Nombre} {props.Apellido1} {props.Apellido2}</h3>
+            <h3>Mi edad es: {props.Edad}</h3>
+            <button onClick={ManejarClic}>Dame clic</button>
         </div>
     )    
 }
