136cbdb8de05f6540146b61ea2bbb32c0a9d77bd
Uluru92 <jorddy.castro@gmail.com>
2024-11-09 23:43:42 -0600

    creacion carpeta components y ordenar

--- Proyectos REACT/mi-primer-app/src/App.js
+++ Proyectos REACT/mi-primer-app/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import MiPrimerComponente from './MiPrimerComponente';
+import MiPrimerComponente from './components/MiPrimerComponente';
 
 function App() {
   return (
