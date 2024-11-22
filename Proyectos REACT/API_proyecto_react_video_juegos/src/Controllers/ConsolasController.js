b80c8a7f1f76c794b398fb703e5de7b8077d8589
Uluru92 <jorddy.castro@gmail.com>
2024-11-21 23:50:03 -0600

    ConsolasControler edited

--- Proyectos REACT/API_proyecto_react_video_juegos/src/Controllers/ConsolasController.js
+++ Proyectos REACT/API_proyecto_react_video_juegos/src/Controllers/ConsolasController.js
@@ -1,118 +1,20 @@
-const personasBD = require("../DataBase/PersonasBD");
+const accesoBD = require("../DataBase/ConsolasBD");
 
 
-const ObtenerTodasLasPersonas = (req, res) =>
-    {
-        const TodasLasPersonas = personasBD.ObtenerTodasLasPersonas();
-        res.send({ DetalleRespuesta:TodasLasPersonas });
-    }
-
-const ValidarCredencialesLogin = (req,res)=>
-    {
-        const informacionUsuario = req.body;
-        const TodasLasPersonas = personasBD.ObtenerTodasLasPersonas();
-        const existeUsuario = TodasLasPersonas.some(tx => tx.userName === informacionUsuario.userName
-                                                        && tx.passWord === informacionUsuario.passWord);
-
-        let codigoDeRespuesta = 0;
-        let descripcionRespuesta = "Acceso Permitido";
-
-        if(!existeUsuario)
-        {
-            codigoDeRespuesta = 99;
-            descripcionRespuesta = "Acceso Denegado";
-        }
-
-        const DetalleRespuesta= 
-        {
-            Codigo : codigoDeRespuesta,
-            Descripcion : descripcionRespuesta
-        }
-
-        res.send(DetalleRespuesta);
-    }
-
-const GuardarPersona = (req, res)=>
-    {
-        const informacionUsuario = req.body;
-        const TodasLasPersonas = personasBD.ObtenerTodasLasPersonas();
-        const existeUsuario = TodasLasPersonas.some(tx => tx.userName === informacionUsuario.userName
-                                                        && tx.passWord === informacionUsuario.passWord);
-                           
-        let codigoDeRespuesta = 98;
-        let descripcionRespuesta = "Usuario ya existe";
-
-        if(!existeUsuario)
-        {
-            personasBD.GuardarPersona(informacionUsuario);
-            codigoDeRespuesta = 0;
-            descripcionRespuesta = "Persona Registrada";
-        }
-
-        const DetalleRespuesta= 
-        {
-            Codigo : codigoDeRespuesta,
-            Descripcion : descripcionRespuesta
-        }
-
-        res.send(DetalleRespuesta);
-    }
-
-const EliminarPersona = (req, res) =>
-    {
-        const informacionUsuario = req.body;
-        const TodasLasPersonas = personasBD.ObtenerTodasLasPersonas();
-        const existeUsuario = TodasLasPersonas.some(tx => tx.userName === informacionUsuario.userName);
-                           
-        let codigoDeRespuesta = 97;
-        let descripcionRespuesta = "Usuario no existe";
-
-        if(existeUsuario)
-        {
-            personasBD.EliminarPersona(informacionUsuario);
-            codigoDeRespuesta = 0;
-            descripcionRespuesta = "Persona Eliminada";
-        }
-
-        const DetalleRespuesta= 
-        {
-            Codigo : codigoDeRespuesta,
-            Descripcion : descripcionRespuesta
-        }
-
-        res.send(DetalleRespuesta);
-    }
-
-const EditarPersona = (req, res) =>
-
+const ObtenerConsolas = (req, res) =>
 {
-    const informacionUsuario = req.body;
-    const TodasLasPersonas = personasBD.ObtenerTodasLasPersonas();
-    const existeUsuario = TodasLasPersonas.some(tx => tx.userName === informacionUsuario.userName);
+    const detalle = accesoBD.ObtenerConsolas();
 
-    let codigoDeRespuesta = 96;
-    let descripcionRespuesta = "Usuario no existe";
-
-    if (existeUsuario) {
-        personasBD.EditarPersona(informacionUsuario);
-        codigoDeRespuesta = 0;
-        descripcionRespuesta = "Persona Editada";
-    }
-
-    const DetalleRespuesta =
+    const DetalleRespuesta = 
     {
-        Codigo: codigoDeRespuesta,
-        Descripcion: descripcionRespuesta
+        Codigo: 0,
+        Descripcion: "Transaccion exitosa",
+        Detalle: detalle
     }
 
-    res.send(DetalleRespuesta);
+    res.send({ DetalleRespuesta});
 }
 
-
 module.exports = {
-    ObtenerTodasLasPersonas,
-    ValidarCredencialesLogin,
-    GuardarPersona,
-    EliminarPersona,
-    EditarPersona
+    ObtenerConsolas
 };
